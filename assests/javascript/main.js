6d2e203924f36e3fc4e0de3d6801959b28566a52
Akhayat <akhayat21@yahoo.com>
2019-03-15 10:37:08 -0400

    added zip to latlng api query

--- assests/javascript/main.js
+++ assests/javascript/main.js
@@ -1,12 +1,13 @@
 var name = "";
 var state = "";
 var city = "";
-var zip = "";
+//var zip = "";
 var type = "";
 var tags = "";
     
-    
+    geocodeQuery();
     breweryQuery();
+    directionsQuery();
     
     
 function breweryQuery() {   
@@ -16,14 +17,40 @@ function breweryQuery() {
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        //console.log(response);
         var breweryName = response[1].name;
-        console.log(breweryName)
+        //console.log(breweryName)
         var breweryLong = response[1].longitude;
-        console.log(breweryLong);
+        //console.log(breweryLong);
         var breweryLat = response[1].latitude;
-        console.log(breweryLat);
-        $(".result").text(breweryName + breweryLat + breweryLong)
+        //console.log(breweryLat);
+        //$(".result").text(breweryName + breweryLat + breweryLong)
+
+    })
+}
+
+
+function geocodeQuery() {   
+    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=32826&key=AIzaSyDKslsrzneWMfjKEnPVY4lhwgqEtK3wgow";
+    
+    console.log(queryURL)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+
+    })
+}
+
+function directionsQuery() {   
+    var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDKslsrzneWMfjKEnPVY4lhwgqEtK3wgow&callback=initMap";
+    console.log(queryURL)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
 
     })
 }
